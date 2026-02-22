5c41b5f7c9b3d35ae0f2a6360605148be1b07572
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-02-22 16:20:33 +0100

    ci: add pocket

---
+++ .pocket/config.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/fredrikaverpil/pocket/pk"
+	"github.com/fredrikaverpil/pocket/tasks/github"
+	"github.com/fredrikaverpil/pocket/tasks/golang"
+)
+
+// Config is the Pocket configuration for this project.
+// Edit this file to define your tasks and composition.
+var Config = &pk.Config{
+	Auto: pk.Serial(
+		golang.Tasks(),
+		github.Tasks(),
+	),
+}
